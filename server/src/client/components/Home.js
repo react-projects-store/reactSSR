d73ee3cd1d32e23e75dfb3931443ce434b0f54e7
mamun-hosen <hossainmamun035@gmail.com>
2020-02-29 22:09:14 +0600

    simple component render from server

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>I'm the very very BEST home Component</div>;
+};
+
+export default Home;
---
+++ server/webpack.server.js
@@ -0,0 +1,35 @@
+const path = require("path");
+module.exports = {
+  /* Inform webpack that we're building a bundle
+  for nodeJS, rather than for the browser */
+  target: "node",
+
+  /* Tell webpack root file of our
+  server application */
+  entry: "./src/index.js",
+
+  /* Tell webpack where to put the output file
+  that is generated */
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+
+  /* Tell webpack to run babel every file it runs through */
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "react",
+            "stage-0",
+            ["env", { targets: { browsers: ["last 2 versions"] } }]
+          ]
+        }
+      }
+    ]
+  }
+};
