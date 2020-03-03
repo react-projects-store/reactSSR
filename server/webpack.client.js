faa417a5e2fc0fc8cee2f6e92e196838b3b8fe3d
mamun-hosen <hossainmamun035@gmail.com>
2020-03-03 18:42:11 +0600

    merging webpack config

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,5 +1,8 @@
 const path = require("path");
-module.exports = {
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+
+const config = {
   /* Tell webpack root file of our
   server application */
   entry: "./src/client/client.js",
@@ -9,23 +12,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
-  },
-
-  /* Tell webpack to run babel every file it runs through */
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+module.exports = merge(baseConfig, config);
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,5 +1,7 @@
 const path = require("path");
-module.exports = {
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+const config = {
   /* Inform webpack that we're building a bundle
   for nodeJS, rather than for the browser */
   target: "node",
@@ -13,23 +15,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  },
-
-  /* Tell webpack to run babel every file it runs through */
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
