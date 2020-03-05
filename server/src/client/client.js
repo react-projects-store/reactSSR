79d9b1c0a74cbb655c3b2d35e9d43b12e431de4c
mamun-hosen <hossainmamun035@gmail.com>
2020-03-05 11:35:49 +0600

    browser router configured for client side

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,13 @@
 // startup point for the client side application.
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+import Routes from "./Routes";
+
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
