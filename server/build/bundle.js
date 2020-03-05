48be3f1650550c1b44a4b567e00db56c06405c3e
mamun-hosen <hossainmamun035@gmail.com>
2020-03-05 12:00:49 +0600

    any kind of request passed by express server

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -88,7 +88,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 app.use(_express2.default.static("public"));
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.send((0, _renderer2.default)(req));
 });
 app.listen(3000, function () {
